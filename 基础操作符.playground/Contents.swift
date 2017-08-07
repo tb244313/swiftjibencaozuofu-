61f1b0c626d0da694c5f3d0d2a27f88af00e048a
apple <1455421519@qq.com>
2017-08-07 17:19:27 +0800

    基本操作符

---
+++ 基础操作符.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+//赋值操作符
+var a = 3
+a = 5
+
+
+//数学操作符
+1 + 2
+3 - 4
+5 * 6
+12 / 3
+13 % 3
+
+
+//比较操作符
+1 > 2
+1 >= 1
+3 < 2
+
+true && true && true
+
+true || false || false
+
+let b = false
+!b
+
